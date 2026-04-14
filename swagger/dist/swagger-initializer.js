3efe9277d582833b97508a915c096d55126ffefc
philipmvitale <philipmvitale@gmail.com>
2026-04-14 08:26:38 -0400

    docs: correct openapi spec location after moving swagger ui files

--- swagger/dist/swagger-initializer.js
+++ swagger/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
-        url: "sleep/src/main/resources/openapi/sleep-api.yaml",
+        url: "../sleep/src/main/resources/openapi/sleep-api.yaml",
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
